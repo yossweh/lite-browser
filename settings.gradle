c7033f134900f4c0c02a3933259ee5b11da7e43e
exelchapo <agentllen1@gmail.com>
2026-06-05 20:21:40 +0800

    Fix settings.gradle (dependencyResolutionManagement)

--- settings.gradle
+++ settings.gradle
@@ -6,7 +6,7 @@ pluginManagement {
     }
 }
 
-dependencyResolution {
+dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
